acb91c7c02878f0f4d9c173504ab4e2c22f97311
Wojciech Padula <wojciech.padula@gmail.com>
2012-11-16 21:32:42 +0100

    Removed junks.

--- _plugins/gist_tag.rb
+++ _plugins/gist_tag.rb
@@ -1,7 +1,3 @@
-;; This buffer is for notes you don't want to save, and for Lisp evaluation.
-;; If you want to create a file, visit that file with C-x C-f,
-;; then enter the text in that file's own buffer.
-
 require 'cgi'
 require 'digest/md5'
 require 'net/https'
